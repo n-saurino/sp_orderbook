a8a432ab0190791b861e72bed00c99213efa2030
Nigel Saurino <nigelsaurino@gmail.com>
2024-04-21 19:21:42 -0400

    Finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "orderbook.h"
+
+using namespace std;
 
 int main(int, char**){
-    std::cout << "Hello, from squarepoint_orderbook!\n";
+    OrderBook book;
+
+    Order first_order = {0, 13.0, 10};
+    Order second_order = {0, 12.0, 15};
+    Order third_order = {1, 13.0, 20};
+    Order final_order = {1, 10.0, 10};
+    book.Add(first_order);
+    book.Add(second_order);
+    book.Add(third_order);
+    book.Add(final_order);
+    book.Print();
+
+
 }
--- orderbook.h
+++ orderbook.h
@@ -13,21 +13,57 @@ struct Order
 
 class OrderBook{
 public:
-    vector<Order> OrderBook::orders;
+    vector<Order> orders;
 
     OrderBook(){
 
     }
 
-    vector<Order>::iterator splitBuysAndSells(){
-
-        return nullptr;
+    void Print(){
+        for(auto order : orders){
+            if(order.isbid){
+                cout << "Buy";
+            }else{
+                cout << "Sell";
+            }
+            cout << " " << order.qty << "@" << order.price << endl;
+        }
     }
 
-    void sortBook(){
+    vector<Order>::iterator splitBuysAndSells() {
+        vector<Order> buys;
+        vector<Order> sells;
+
+        // Split orders into buys and sells
+        for (const auto& order : orders) {
+            if (order.isbid) {
+                buys.push_back(order);
+            } else {
+                sells.push_back(order);
+            }
+        }
+
+        // Copy buys and sells back into the original vector
+        auto it = orders.begin();
+        it = copy(buys.begin(), buys.end(), it);
+        copy(sells.begin(), sells.end(), it);
+
+        // Return an iterator to the beginning of sells
+        return orders.begin() + buys.size();
+    }
+
+    void sortBook() {
         auto it = splitBuysAndSells();
-        sort(orders.begin(), it);
-        sort(it, orders.end());
+
+        // Sort buys from highest to lowest
+        sort(orders.begin(), it, [](const Order& lhs, const Order& rhs) {
+            return lhs.price > rhs.price;
+        });
+
+        // Sort sells from lowest to highest
+        sort(it, orders.end(), [](const Order& lhs, const Order& rhs) {
+            return lhs.price < rhs.price;
+        });
     }
 
     ~OrderBook(){
@@ -40,9 +76,18 @@ public:
 
         if(other == end(orders)){
             orders.push_back(order);
+            sortBook();
         }else{
-
-            orders.erase(other);
+            // cout << other->isbid << " " << other->qty << "@" << other->price << endl;
+            if(order.qty == other->qty){
+                orders.erase(other);
+            }else if(order.qty < other->qty){
+                other->qty -= order.qty;
+            }else if(order.qty > other->qty){
+                order.qty -= other->qty;
+                orders.erase(other);
+                Add(order);
+            }
         }
 
     }
@@ -53,17 +98,16 @@ public:
         size_t i = 0;
         while(i < orders.size()){
             if(order.isbid != orders[i].isbid){
-                if(order.isbid && order.price > orders[i].price){
+                if(order.isbid && order.price >= orders[i].price){
                     ret = orders.begin() + i;
                     return ret;
-                }else if(!order.isbid && order.price < orders[i].price){
+                }else if(!order.isbid && order.price <= orders[i].price){
                     ret = orders.begin() + i;
                     return ret;
                 }
             }
             ++i;
         }
-
         return ret;
     }
 
